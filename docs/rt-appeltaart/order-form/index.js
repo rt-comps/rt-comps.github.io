89ba1f2cf0ca33aecec8da6ca18b936751a50805
Simon Vickers <simon.vickers@roads-technology.nl>
2022-11-09 16:51:29 +0100

    still works?

--- docs/rt-appeltaart/order-form/index.js
+++ docs/rt-appeltaart/order-form/index.js
@@ -1,17 +1,16 @@
 // ===== Load component plus any dependancies
-
 // Import library functions
 import * as rt from "../../rt.js";
 
-// List dependencies
+// List of dependencies
 const components = ["order-item-container"/*,"order-button","order-item"*/];
 
+// Start timer
 console.time(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
-
 // Load all dependencies
 Promise.all(
   components.map((componentName) => import(`../${componentName}/index.js`))
-  ).then((modules) => {
+  ).then(() => {
     console.timeEnd(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
     // Load this component, specifying preferred version
     rt.loadComponent(import.meta.url, "order-form_v2");
