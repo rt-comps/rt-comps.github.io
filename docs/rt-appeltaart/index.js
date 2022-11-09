cd29b3a34b304174c3f61cb534eb984fe200e29a
Simon Vickers <simon.vickers@roads-technology.nl>
2022-11-09 14:12:08 +0100

    asds

--- docs/rt-appeltaart/index.js
+++ docs/rt-appeltaart/index.js
@@ -4,7 +4,7 @@ console.time(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
 const basePath=`https://${import.meta.url.split('/')[2]}`;
 console.log(import.meta.url.split('/'));
 console.log(import.meta.url.split('/').slice(2));
-const {testPath1,testPath2}=import.meta.url.split('/').slice(2);
+const [testPath1,testPath2]=import.meta.url.split('/').slice(2);
 console.log(`testPath1: ${testPath1}`);
 console.log(`testPath2: ${testPath2}`);
 
@@ -13,7 +13,7 @@ const components = ["order-form"];
 
 // Load all required top-level components
 Promise.all(
-  components.map((componentName) => import(`${basePath}/${componentName}/index.js`))
+  components.map((componentName) => import(`./${componentName}/index.js`))
 ).then((modules) => {
   console.timeEnd(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
   //modules.forEach((module) => console.log("loaded", modules));
