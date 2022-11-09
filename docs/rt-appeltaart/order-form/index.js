a91bd283adc6f3cd3412ce56f72485aafbd13c58
Simon Vickers <simon.vickers@roads-technology.nl>
2022-11-09 16:26:39 +0100

    Tidied up.  Fixed?

--- docs/rt-appeltaart/order-form/index.js
+++ docs/rt-appeltaart/order-form/index.js
@@ -11,12 +11,10 @@ console.time(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
 // Load all dependencies
 Promise.all(
   components.map((componentName) => import(`../${componentName}/index.js`))
-).then((modules) => {
-  console.timeEnd(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
-  //modules.forEach((module) => console.log("loaded", modules));
-});
-
-console.log("about to load order-form_v2");
-
-// Load this component, specifying preferred version
-rt.loadComponent(import.meta.url, "order-form_v2");
+  ).then((modules) => {
+    console.timeEnd(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
+    // Load this component, specifying preferred version
+    rt.loadComponent(import.meta.url, "order-form_v2");
+  });
+  
+  
