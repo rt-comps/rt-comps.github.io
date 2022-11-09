0b189e614d0aae9328bde7f600c0ccec76783f82
Simon Vickers <simon.vickers@roads-technology.nl>
2022-11-09 14:19:20 +0100

    asd

--- docs/rt-appeltaart/index.js
+++ docs/rt-appeltaart/index.js
@@ -1,19 +1,16 @@
 // ===== Import all required top-level components 
 //  components will be loaded in parallel, so load order cannot be depended on.
 console.time(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
-const basePath=`https://${import.meta.url.split('/')[2]}`;
 console.log(import.meta.url.split('/'));
 console.log(import.meta.url.split('/').slice(2));
-const [testPath1,testPath2]=import.meta.url.split('/').slice(2);
-console.log(`testPath1: ${testPath1}`);
-console.log(`testPath2: ${testPath2}`);
-
+const [hostPath,compPath]=import.meta.url.split('/').slice(2);
+const basePath=`https://${hostPath}/${compPath}`;
 // List all required top-level components
 const components = ["order-form"];
 
 // Load all required top-level components
 Promise.all(
-  components.map((componentName) => import(`./${componentName}/index.js`))
+  components.map((componentName) => import(`${basePath}/${componentName}/index.js`))
 ).then((modules) => {
   console.timeEnd(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
   //modules.forEach((module) => console.log("loaded", modules));
