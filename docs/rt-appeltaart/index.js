2f1582b9a6dfe5f92b1f9dd55a0b3ca9e24912d2
JimBob <si@simonvickers.net>
2022-11-09 14:08:12 +0100

    qweq

--- docs/rt-appeltaart/index.js
+++ docs/rt-appeltaart/index.js
@@ -4,8 +4,9 @@ console.time(`loadModules for ${import.meta.url.split('/').slice(-2)[0]}`);
 const basePath=`https://${import.meta.url.split('/')[2]}`;
 console.log(import.meta.url.split('/'));
 console.log(import.meta.url.split('/').slice(2));
-const testPath=import.meta.url.split('/').slice(2);
-console.log(`testPath: ${testPath}`);
+const {testPath1,testPath2}=import.meta.url.split('/').slice(2);
+console.log(`testPath1: ${testPath1}`);
+console.log(`testPath2: ${testPath2}`);
 
 // List all required top-level components
 const components = ["order-form"];
