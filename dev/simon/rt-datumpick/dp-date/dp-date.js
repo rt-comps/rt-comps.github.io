ec0353d58d325cc996de7732d1d9ed5d892f0c13
JimBob <si@simonvickers.net>
2023-09-22 17:44:55 +0200

    updated dp with formAssoc and removed inter-depend

--- dev/simon/rt-datumpick/dp-date/dp-date.js
+++ dev/simon/rt-datumpick/dp-date/dp-date.js
@@ -7,36 +7,43 @@ const [compName] = rtlib.parseURL(import.meta.url);
 
 customElements.define(compName,
     class extends rtBC.RTBaseClass {
+        // Private class fields
+        #_sR;                   // Holds shadowRoot node
+        #_locale;               // What locale to use for text display 
+        #_date = new Date();    // Date object holding current date
+
         //+++ Lifecycle Events
         //--- constructor
         constructor() {
-            const _sR = super().attachShadow({ mode: "open" })
-            _sR.append(this.$getTemplate());
+            super();
+            this.#_sR = this.attachShadow({ mode: "open" })
+            this.#_sR.append(this.$getTemplate());
 
-            // Initialise local properties
-            this.$week = 0;     // Weeks relative to current week
-            this.$date = 0;     // Current date value for this component
-            this.$month = 0;    // Current month value for this component
+            // Get value of 'locale' attribute and leave undefined if getAttribute() returns null
+            this.#_locale = this.getAttribute('locale') || undefined;
 
-            // Determine day string for this instance, using 'day' attribute
-            _sR.querySelector('#day').innerHTML = (this.hasAttribute('day') ? rtlib.getDay(parseInt(this.getAttribute('day')) % 7) : 'ERROR');
-            // Display intitial date text
-            this.render();
-
-            //*** Listener
-            //--- for 'onClick' - dispatch 'datepicked' outside the shadow root
-            _sR.querySelector('#container').addEventListener('click', () => this.pickDate());
+            //--- Respond to 'click' event
+            this.#_sR.querySelector('#container').addEventListener('click', () => this.pickDate());
         }
 
         //--- connectedCallback
         connectedCallback() {
-            //*** Listeners
-            // Placed here to ensure 'parentNode' is not null
-            if (this.parentNode) this.parentNode.addEventListener('changeWeek', (e) => this.changeWeek(e));
+            setTimeout(() => {
+                if (this.parentNode) {
+                    //--- Re-render if notified that parentNode._week has been modified
+                    this.parentNode.addEventListener('changeWeek', () => this.render());
+                    //--- Reset 'chosen' CSS when notified that a change in choice has been made
+                    this.parentNode.addEventListener('choiceMade', (e) => this.reset(e));
+                    // Prefer locale of parent node if defined
+                    this.#_locale = this.parentNode._locale || this.#_locale;
+                    // Calculate initial date text
+                    this.render()
+                } else console.error(`${this.localName} - Could find parent`);
+            });
         }
 
         //--- attributeChangedCallback
-        // Ensures component responds to an 'invalid' attribute being assigned during parent component loading
+        // Re-render if the invalid attribute is altered
         static get observedAttributes() { return ['invalid'] };
         attributeChangedCallback() { this.render(); }
         //+++ End of Lifecycle Events
@@ -48,44 +55,55 @@ customElements.define(compName,
         // Calculate the correct date text and state for this component
         render() {
             if (this.hasAttribute('day')) {
-                const _sR = this.shadowRoot;
-                // Determine today's date
+                // Start by determining today's date
                 const today = new Date();
                 // A useful value
                 const dateId = parseInt(this.getAttribute('day'));
-                // Determine if date is possible and render in grey if not
-                _sR.querySelector('#container').style.color = ((this.hasAttribute('invalid') || (this.$week === 0 && (today.getDay() > dateId))) ? "#CCCCCC" : "#000000");
-                // Modify "today" to the correct date for the button in a way that handles a month boundary when required
-                today.setDate(today.getDate() + (7 * this.$week) + (dateId - today.getDay()));
-                // Update day & date properties
-                this.$date = today.getDate();
-                this.$month = today.getMonth();
-                // Render date
-                _sR.querySelector('#date').innerHTML = `${this.$date} ${rtlib.getMonth(this.$month)}`
+
+                // Disable date if calculated as in the past or labelled as invalid - color used as enable/disable flag
+                this.#_sR.querySelector('#container').style.color = ((this.hasAttribute('invalid') || (this.parentNode._week === 0 && (today.getDay() > dateId))) ? "rgb(204,204,204)" : "rgb(0,0,0)");
+
+                /// Determine what the date object value for this instance should be
+                // Reset #_date to today
+                this.#_date = today;
+                // Modify #_date to the correct value in a way that handles a month boundary when required
+                this.#_date.setDate(today.getDate() + (7 * this.parentNode._week) + (dateId - today.getDay()));
+
+                /// Perform value rendering
+                // localised day name
+                this.#_sR.querySelector('#day').innerHTML = this.$localeDate(this.#_date, this.#_locale, { weekday: 'short' });
+                // localised date
+                this.#_sR.querySelector('#date').innerHTML = `${this.#_date.getDate()} ${this.$localeDate(this.#_date, this.#_locale, { month: 'short' })}`;
+            } else {
+                // What do render when there is no 'day' attribute
+                this.#_sR.querySelector('#day').innerHTML = 'ERROR';
+                this.#_sR.querySelector('#date').innerHTML = '...';
             }
         }
 
-        //--- changeWeek
-        // Update the week when arrow pressed
-        changeWeek(e) {
-            // Recover week change value from event detail
-            this.$week += e.detail.change;
-            // Limit lowest value to zero
-            if (this.$week < 0) this.$week = 0;
-            // Adhere to upper limit - if set
-            if (this.parentNode.$maxWeek && this.$week > this.parentNode.$maxWeek) this.$week = this.parentNode.$maxWeek;
-            // Recalculate text fields
-            this.render();
-        }
-
         //--- pickDate
-        // Send composed event with current date and month data if date is valid
+        // Respond to click on component by sending an event holding date data ...unless date is disabled
         pickDate() {
-            if (this.shadowRoot.querySelector('#container').style.color == "rgb(0, 0, 0)") {
+            const cont = this.#_sR.querySelector('#container');
+            // Check this component is enabled
+            if (cont.style.color == "rgb(0, 0, 0)") {
+                // Highlight choice
+                cont.classList.add('chosen');
+                // Send choice
                 this.$dispatch({
                     name: 'datepicked',
-                    detail: { date: this.$date, month: this.$month, day: this.getAttribute('day') }
-                })
+                    detail: {
+                        date: this.#_date,
+                        day: this.getAttribute('day')
+                    }
+                });
+            }
+        }
+        //--- reset
+        // Reset chosen CSS if this is not the chosen element
+        reset(e) {
+            if (!this.hasAttribute('invalid')) {
+                if (this.getAttribute('day') !== e.detail.day) this.#_sR.querySelector('#container').classList.remove('chosen');
             }
         }
     }
