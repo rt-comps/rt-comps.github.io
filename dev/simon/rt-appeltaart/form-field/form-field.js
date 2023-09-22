7f3948bf329243d3b782ffbda8e93c0b11e8183c
Simon Vickers <simon.vickers@roads-technology.nl>
2023-09-22 17:56:09 +0200

    Improved form behaviour

--- dev/simon/rt-appeltaart/form-field/form-field.js
+++ dev/simon/rt-appeltaart/form-field/form-field.js
@@ -19,30 +19,72 @@ customElements.define(compName,
         constructor() {
             super();
             // Create and build Shadow DOM
-            const _sR = this.attachShadow({ mode: "open" });
+            const _sR = this.attachShadow({ mode: "open", delegateFocus: true });
             _sR.append(this.$getTemplate());
 
-            // Allow access to form 
+            // Allow form participation 
             this.#_internals = this.attachInternals();
 
             // Useful Nodes
             this.#_input = _sR.querySelector('input');
 
-            // Add asterisk to 'required' fields
-            if (this.hasAttribute('required')) _sR.querySelector('span').innerHTML = '&nbsp*';
+            // Handle 'required' fields
+            if (this.hasAttribute('required')) {
+                this.#_input.setAttribute('required', '');
+                _sR.querySelector('span').innerHTML = '&nbsp*';
+            }
+
+            // Handle input type and/or contraints
+            if (this.hasAttribute('type')) {
+                const inputType = this.getAttribute('type');
+                // Set contraints if type specified
+                switch (inputType) {
+                    case 'tel':
+                        // constrain phone number to exactly 10 digits starting with zero(0)
+                        this.#_input.pattern = "0[0-9]{9}";
+                    case 'email':
+                        this.#_input.type = inputType;
+                        break;
+                    case 'post':
+                        // constrain postcode to 4 digits and 2 alpha - with or without space
+                        this.#_input.pattern = "[0-9]{4} {0,1}[A-Za-z]{2}";
+                        break;
+                }
+            }
 
             // Use 'label' attribute for field label
             _sR.querySelector('label').insertAdjacentHTML('afterbegin', `${this.getAttribute('label') || 'Name Missing'}&nbsp;`);
         }
+
         //--- formAssociatedCallback
         // triggered when component is associated with (or dissociated from) a form
         formAssociatedCallback() {
             // If this is an association then add listener for formData request
             if (this.#_internals.form) {
-               this.#_internals.form.addEventListener('formdata', (e) => e.formData.append(this.getAttribute('name'), this.#_input.value));
+                // Set form values when the FormData() contructor is invoked (via submit or new)
+                this.#_internals.form.addEventListener('formdata', (e) => e.formData.set(this.name, this.value));
             }
+        }
+
+        //--- formResetCallback
+        // respond to the enclosing form being reset
+        formResetCallback() {
+            this.#_input.value = null;
         }
         //+++ End of Lifecycle Events
 
+        // Expose some standard form element properties and methods
+        get value() { return this.#_input.value; }
+        //set value(v) { this.#_input.value = v; }
+
+        get name() { return this.getAttribute('name'); }
+        //get form() { return this.#_internals.form; }
+        //get validity() { return this.#_internals.validity; }
+
+        // Check validity of value provided
+        checkValidity() { return this.#_input.checkValidity(); }
+        //reportValidity() { return this.#_internals.reportValidity(); }
+        // When focus is pushed to component then push focus to input field
+        focus() { this.#_input.focus() };
     }
 );
