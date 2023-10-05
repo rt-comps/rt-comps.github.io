797507b5ce35097522367ed91584b61ef0832df7
JimBob <si@simonvickers.net>
2023-10-05 11:25:43 +0200

    Re-write of cart button vis, add detail saving

--- dev/simon/rt-appeltaart/order-form/order-form_v5.js
+++ dev/simon/rt-appeltaart/order-form/order-form_v5.js
@@ -9,45 +9,41 @@ customElements.define(compName,
   class extends rtBC.RTBaseClass {
     // Declare private class fields
     #_sR;
+    #_form;
 
     //+++ Lifecycle Events
     //--- constructor
     constructor() {
+      // Initialise 'this'
       super();
-      this.#_sR = this.attachShadow({ mode: "open" })
+      // Attach shadowDOM
+      this.#_sR = this.attachShadow({ mode: "open" });
+      // Attach contents of template placed in document.head
       this.#_sR.append(this.$getTemplate());
 
       //### Retrive form sizes and define defaults
       const attributes = this.attributes;
       this.#_sR.querySelector('#container').style.width = attributes['form-width'] ? attributes['form-width'].value : '1000px';
-      // this.#_sR.querySelector('#container').style.height = attributes['form-height'] ? attributes['form-height'].value : '600px';
       const cartWidth = attributes['cart-width'] ? attributes['cart-width'].value : '350px';
       const _cart = this.#_sR.querySelector('#cart');
       _cart.style.flex = `0 0 ${cartWidth}`;
       _cart.style.maxWidth = cartWidth;
-      this.setAttribute('id', 'eventBus');
+      this.id = 'eventBus';
 
       //### Event Listeners
-      const menuNode = this.#_sR.querySelector('#menu-container');
-      //___ updatemenu - Respond to product choice 
-      menuNode.addEventListener('updatemenu', (e) => this.updateData(e));
+      //___ updatemenu - Respond to product choice
+      this.addEventListener('updatemenu', (e) => this.updateItemData(e));
       //___ updatecount - Respond to +/- presses when choosing items
-      menuNode.addEventListener('updatecount', (e) => this.displayDetailButton(e));
+      this.#_sR.querySelector('#menu-container').addEventListener('updatecount', (e) => this.displayDetailButton(e));
       //___ cartmod - Respond to +/- presses to items in cart
-      this.#_sR.querySelector('#cart')
-        .addEventListener('cartmod', (e) => this.modifyCart(e));
+      this.#_sR.querySelector('#cart').addEventListener('cartmod', (e) => this.modifyCart(e));
       //___ add-items_click - Add the currently selected items to the cart
-      this.#_sR.querySelector('#add-but')
-        .addEventListener('click', () => this.addToCart());
+      this.#_sR.querySelector('#add-but').addEventListener('click', () => this.addToCart());
       //___ place-order_click - Send completed order to out of form
-      this.#_sR.querySelector('#further-but')
-        .addEventListener('click', () => this.continueOrder());
+      this.#_sR.querySelector('#further-but').addEventListener('click', () => this.continueOrder());
       //___ recover-order_click - Fill cart with the items from the last order
-      this.#_sR.querySelector('#recover-but')
-        .addEventListener('click', () => this.recoverOrder());
-      this.#_sR.querySelector('#sub-but').addEventListener('click', () => this.#_sR.querySelector('form').requestSubmit());
-      this.#_sR.querySelector('form').addEventListener('submit', (e) => this.validate(e));
-
+      this.#_sR.querySelector('#recover-but').addEventListener('click', () => this.recoverOrder());
+      this.#_sR.querySelector('#sub-but').addEventListener('click', () => this.dispatchOrder());
     }
 
     //--- connectedCallback
@@ -59,9 +55,14 @@ customElements.define(compName,
     //+++ End of Lifecycle Events
 
 
-    //--- processMenuItems
-    // Create the pictoral menu at the top of the order form based on form HTML
+    //--- initialiseMenu
+    // Performs the following actions
+    // - Create the pictoral menu at the top of the order form based on form HTML
+    // - Restore cart if there were previously items present
+    // - Move form HTML into the shadowDOM
+    // - Prepopulate form if saved details found
     initialiseMenu() {
+      /// Create pictorial menu
       // Recover image path from setting in HTML
       const imgPath = this.querySelector("form-config span#imgpath").innerHTML;
 
@@ -82,18 +83,20 @@ customElements.define(compName,
         })
       }));
 
+      /// Restore cart
       if (localStorage.getItem('currentOrder')) this.updateCart();
       this.displayCartButtons();
 
-      // Move user's form to Shadow DOM
+      /// Move user's form to shadowDOM
       const srcNode = this.querySelector('div[slot="user-details"]');
-      const dstNode = this.shadowRoot.querySelector('form[id="details-form"]');
-      dstNode.append(...srcNode.children);
+      this.#_form = this.shadowRoot.querySelector('form[id="details-form"]');
+      this.#_form.append(...srcNode.children);
+
     }
 
-    //--- updateData
+    //--- updateItemData
     // Display requested data (updatemenu event) or clear data (called manually)
-    updateData(e) {
+    updateItemData(e) {
       // Handle event if present
       let newData;
       if (e) {
@@ -123,15 +126,17 @@ customElements.define(compName,
     displayDetailButton(e) {
       if (e) e.stopPropagation();
       // Get button node
-      const buttonNode = this.shadowRoot.querySelector('#details #add-but');
+      const buttonNode = this.shadowRoot.querySelector('#add-but');
       // Determine button appearance based on whether anything has been selected for addition
       if (this.querySelectorAll('item-data[slot] item-line[count]').length === 0) {
         // When no selection, button is grey and says 'cancel'
-        buttonNode.style.backgroundColor = 'rgb(128, 128, 128)';
+        buttonNode.classList.remove('buttonadd');
+        // buttonNode.style.backgroundColor = 'rgb(128, 128, 128)';
         buttonNode.innerHTML = 'Annuleren';
       } else {
         // When something selected, button reverts to style and says 'Add'
-        buttonNode.removeAttribute('style');
+        buttonNode.classList.add('buttonadd');
+        // buttonNode.removeAttribute('style');
         buttonNode.innerHTML = 'Toevoegen';
       }
     }
@@ -150,16 +155,20 @@ customElements.define(compName,
       //Choose which button to display
       let newBut;
       switch (true) {
-        case (this.querySelectorAll('line-item[slot="cart"][count]').length === 0 || localStorage.getItem('lastOrder') || fiddle):
+        case ((this.querySelectorAll('line-item[slot="cart"][count]').length === 0 || fiddle) && localStorage.getItem('lastOrder') !== null):
           newBut = 'last';
           break;
         case this.#_sR.querySelector('#form-container').style.visibility === 'visible':
           newBut = 'order';
           break;
-        default:
+        case (this.querySelectorAll('line-item[slot="cart"][count]').length !== 0):
           newBut = 'further';
+          break;
+        default:
+          newBut = null;
       }
-      buttons[newBut].style.display = '';
+      // Display the correct button - if there is one
+      if (newBut) buttons[newBut].style.display = '';
     }
 
     //--- updateCart
@@ -219,8 +228,7 @@ customElements.define(compName,
       // itemObj must contain an action property
       if (itemObj.action) {
         // Parse from currently stored value array from JSON or empty array if item doesn't exist
-        const currentItems = JSON.parse(localStorage.getItem('currentOrder') || "[]");
-
+        const currentItems = JSON.parse(localStorage.getItem('currentOrder')) || [];
         // Let's assume this is a new value
         let notFound = true;
 
@@ -241,7 +249,6 @@ customElements.define(compName,
             }
           }
         }
-
         // If no existing entry found this must be new item to add to the array
         if (notFound) {
           // Remove the 'action'property from the object
@@ -285,39 +292,27 @@ customElements.define(compName,
       });
 
       // Whether closing or adding, current item data is cleared
-      this.updateData();
+      this.updateItemData();
     }
 
     //--- continueOrder
     continueOrder() {
-      this.shadowRoot.querySelector('#form-container').style.visibility = 'visible';
-      this.displayCartButtons();
-    }
-
-    //--- dispatchOrder
-    // Dispatch the current order in an event and reset the form
-    dispatchOrder() {
-      // Reset form
-
-      // Get current order from local storage
-      const newOrder = localStorage.getItem('currentOrder');
-      // Set last order on the user's local storage to current order
-      localStorage.setItem('lastOrder', newOrder);
-      // Dispatch an event containing the order details
-      this.$dispatch({
-        name: 'neworder',
-        detail: {
-          orderdetails: newOrder
+      /// Prepopulate form if saved details found
+      const details = localStorage.getItem('userDeets');
+      if (details) {
+        // Set 'savefields' boolean
+        this.#_form.querySelector('#savefields').checked = true;
+        // Get stored details
+        const deetsObj = JSON.parse(details);
+        // Repopulate details in to form
+        for (const [key, value] of Object.entries(deetsObj)) {
+          this.#_form.querySelector(`[name=${key}]`).value = value;
         }
-      });
-
-      // ### Clean up
-      // Hide the details form
-      this.shadowRoot.querySelector('#form-container').style.visibility = '';
-      // Clear the cart
-      localStorage.removeItem('currentOrder');
-      this.updateCart();
-      //  Reset the buttons
+      }
+      
+      // Unhide form
+      this.shadowRoot.querySelector('#form-container').style.visibility = 'visible';
+      // Update button visibility in cart
       this.displayCartButtons();
     }
 
@@ -325,37 +320,78 @@ customElements.define(compName,
     // Reload the last order dispatched
     recoverOrder() {
       // Fill the currentOrder storage item with the contents from lastOrder
-      localStorage.setItem('currentOrder', localStorage.getItem('lastOrder'))
+      if (localStorage.getItem('lastOrder')) localStorage.setItem('currentOrder', localStorage.getItem('lastOrder'))
       // Rebuild cart
       this.updateCart();
     }
 
-    //--- validate
+    //--- dispatchOrder
     // Catch the form submit event and ensure required values have been provided
-    validate(e) {
-      // Stop default validation check
-      e.preventDefault();
+    dispatchOrder() {
       // Get form node
       const form = this.shadowRoot.querySelector('#details-form');
       // Continue if form found
       if (form) {
-        const formValues = new FormData(form);
-        console.log([...formValues.entries()]);
-        //        for (const [key, value] of formValues.entries()) console.log(key, value);
-      }
-      else console.warn('Form not found');
+        //### Check form data validity
+        // Set fail flag
+        let firstFail = false;
 
-      // let valid = true;
-      // const reqNodes = [...this.querySelectorAll('form-field[required]')]
-      // for (const el in reqNodes) {
-      //   console.log(reqNodes[el]);
-      //   if (!reqNodes[el].shadowRoot.querySelector('input').value) {
-      //     console.log(`Could not find value for ${reqNodes[el].getAttribute('name')}`);
-      //     break;
-      //   }
-      // }
-      console.log('Form Submitted!');
-      this.dispatchOrder();
+        // Collect all possible field types
+        const nodes = form.querySelectorAll('form-field, pickup-locations, date-picker');
+        // Check validity of each field type and set focus to first field to fail check
+        for (const el of nodes) {
+          // console.log(`${el.localName}: ${el.checkValidity()}`);
+          if (!el.checkValidity()) {
+            if (!firstFail) {
+              el.focus();
+              firstFail = true;
+            }
+          }
+        }
+
+        // Disptach order if all checks pass
+        if (!firstFail) {
+          //### Dispatch order and reset form
+          // Collect the current form data
+          const formValues = new FormData(form);
+          //         console.log([...formValues.entries()]);
+          // Get current order from local storage
+          const newOrder = localStorage.getItem('currentOrder');
+          // Dispatch an event containing the order details
+          this.$dispatch({
+            name: 'neworder',
+            detail: {
+              person: Object.fromEntries(formValues.entries()),
+              order: JSON.parse(newOrder)
+            }
+          });
+
+          // ### Clean up
+          // Hide the details form
+          this.shadowRoot.querySelector('#form-container').style.visibility = '';
+          // Set last order on the user's local storage to current order
+          localStorage.setItem('lastOrder', newOrder);
+          // Clear the cart
+          localStorage.removeItem('currentOrder');
+          this.updateCart();
+          //  Reset the buttons
+          this.displayCartButtons();
+          // Check if user has chosen to save their details
+          const saveFields = this.#_sR.querySelector('#savefields');
+          // If yes then put details in object and store as JSON string
+          if (saveFields && saveFields.checked) {
+            const fields = [...this.#_sR.querySelectorAll(`#details-form form-field,textarea`)];
+            const output = fields.reduce((acc, el) => ({ ...acc, [el.name]: el.value }), {});
+            localStorage.setItem('userDeets', JSON.stringify(output));
+            // If no then clear any existing data
+          } else localStorage.removeItem('userDeets');
+          form.reset();
+
+
+          console.log('Form Submitted!');
+        }
+      }
+      else console.warn('Form submission failed: form not found');
     }
   }
 );
