71e736e71af08c5388d67a30c6238117e81808a6
JimBob <si@simonvickers.net>
2023-10-05 11:23:28 +0200

    Re-factored for PCFs and tidied

--- dev/simon/rt-appeltaart/item-variety/item-variety_v5.js
+++ dev/simon/rt-appeltaart/item-variety/item-variety_v5.js
@@ -5,37 +5,46 @@ const [compName] = rtlib.parseURL(import.meta.url);
 customElements.define(
     compName,
     class extends rtBC.RTBaseClass {
-        //+++ Lifecycle events
-        // --- Contructor
-        constructor() {
-            // Attach contents of template placed in document.head
-            const _sR = super().attachShadow({ mode: "open" });
-            _sR.append(this.$getTemplate());
-            this._lines = _sR.querySelector('#lines')
+        // Declare private class fields
+        #_sR;
+        #_lines;
 
-            // Respond to user click to toggle variety display
-            _sR.querySelector('#title').addEventListener('click', () => this._lines.hidden = this._lines.hidden ? false : true);
+        //+++ Lifecycle events
+        //--- Contructor
+        constructor() {
+            // Initialise 'this'
+            super();
+            // Attach shadowDOM
+            this.#_sR = this.attachShadow({ mode: "open" });
+            // Attach contents of template placed in document.head
+            this.#_sR.append(this.$getTemplate());
+
+            // Create handle to slotted content for toggling visibility
+            this.#_lines = this.#_sR.querySelector('#lines')
+
+            // Set displayed title to combination of 'value' and 'desc' (if provided)
+            this.#_sR.querySelector('#title').innerHTML = `${this.$attr('value')}${this.hasAttribute('desc') ? ' ' + this.$attr('desc') : ''}`;
+
+            // Respond to user click to toggle variety items display
+            this.#_sR.querySelector('#title').addEventListener('click', () => this.#_lines.hidden = !this.#_lines.hidden);
         }
 
-        // --- connectedCallback
+        //--- connectedCallback
         connectedCallback() {
-            const _sR = this.shadowRoot;
-            // this.findEventBus();
-            // Set displayed title to combination of 'value' and 'desc' (if provided)
-            _sR.querySelector('#title').innerHTML =
-                `${this.$attr('value')}${this.hasAttribute('desc') ? ' ' + this.$attr('desc') : ''}`;
-            // if (this.hasAttribute('default')) _sR.querySelector('#lines').removeAttribute('style');
-            this.initialiseDisplay(_sR.querySelector('#lines'));
-            this.closest('#eventBus').addEventListener('updatemenu', (e) => this.initialiseDisplay(this._lines, e));
+            // Delay listener attachment to ensure '#eventBus' is present
+            setTimeout(() => {
+                const eventBus = this.closest('#eventBus');
+                if (eventBus) eventBus.addEventListener('updatemenu', (e) => this.initialiseDisplay(e));
+                else console.error(`${this.tagName}: Element with id of "eventBus" not found`);
+            });
         }
         //+++ End Of Lifecycle events
 
-
-        initialiseDisplay(_target, e) {
-            // Ensure 'default' attribute is respected every time the product info is opened
-            if (!e || e.detail.id === this.parentNode.id) {
-                // Set status of variety when a product is selected
-                if (this.hasAttribute('default')) _target.hidden = false;
+        //--- initialiseDisplay
+        // Ensure 'default' variety attribute is respected every time this menu item is chosen
+        initialiseDisplay(e) {
+            if (e.detail.id === this.parentNode.id) {
+                this.#_lines.hidden = this.hasAttribute('default') ? false : true;
             }
         }
     }
