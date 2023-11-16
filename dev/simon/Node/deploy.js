cf0e766f2215189540b32cb9ad3046a640051877
Simon Vickers <simon.vickers@roads-technology.nl>
2023-11-16 16:41:43 +0100

    SV end of day

--- dev/simon/Node/deploy.js
+++ dev/simon/Node/deploy.js
@@ -34,7 +34,6 @@ try {
         fs.readdirSync(path, { withFileTypes: true }).forEach(
             // Add each list item to the array with path
             (item) => {
-                const thisRes = `${path}/${item.name}`;
                 // If item is directory then walk this new directory
                 if (item.isDirectory()) walk(`${path}/${item.name}`, result)
                 // If item is a file then ad to list
@@ -50,14 +49,12 @@ try {
     // Assume script is called from within project dir structure and determine the
     // base path from that
     const execPath = process.cwd();
-    console.log(execPath);
     if (execPath.indexOf('github.io') < 0) throw new Error('No project directory not found.  Ensure script is run from within project directory structure', { cause: 'custom' });
-    const compPath = execPath.slice(0, execPath.indexOf('github.io') + 9);
-    console.log(compPath);
-    const devPath = `${compPath}/dev/simon`;
+    const basePath = execPath.slice(0, execPath.indexOf('github.io') + 9);
+    const devPath = `${basePath}/dev/simon`;
     const srcPath = `${devPath}/${comp}`;
     const stgPath = `${devPath}/tmp`;
-    const dstPath = `${compPath}/docs`;
+    const dstPath = `${basePath}/docs`;
 
     // ### Pre-flight checks
     // Check that the component can be found  
@@ -67,34 +64,55 @@ try {
 
     // ### Main Code
     const pathOpt = { recursive: true };
-    // const readOpt = { encoding };
 
     // Create array of files to process
     const files = walk(srcPath);
     // Process all files in array
     for (let file of files) {
         // Create staging path required for this file if it has not been previously created
-        const filePath = `${stgPath}${file.slice(0, file.lastIndexOf('/'))}`;
-        if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, pathOpt);
+        // const filePath = `${stgPath}${file.slice(0, file.lastIndexOf('/'))}`;
+        // if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true });
         // Process file based on type
         const fileType = file.substring(file.lastIndexOf('.') + 1);
         switch (fileType) {
             // Use uglify-js with default settings for JS
             case 'js':
-                fs.writeFileSync(`${stgPath}${file}`, uglify(fs.readFileSync(`${devPath}${file}`, 'utf8')).code);
+                // Find substitutions when moving dev to prod
+                // Get file contents
+                const contents = fs.readFileSync(`${devPath}${file}`, 'utf8');
+                // Check if any required substitions
+                if (contents.indexOf('ForProd') > -1) {
+                    console.log(file);
+                    // Find any occurences in file and merge in to single object
+                    let subs = contents.match(/ForProd\:.*/g).reduce((acc,el)=>{
+
+                        const temp = JSON.parse(element.slice(9));
+                   
+                    }, {})
+                }
+
+                // fs.writeFileSync(`${stgPath}${file}`, uglify(fs.readFileSync(`${devPath}${file}`, 'utf8')).code);
                 break;
             // Use html-minifier for HTML
             case 'html':
             case 'htm':
-                fs.writeFileSync(`${stgPath}${file}`, mini(fs.readFileSync(`${devPath}${file}`, 'utf8'), miniOpt));
+                // fs.writeFileSync(`${stgPath}${file}`, mini(fs.readFileSync(`${devPath}${file}`, 'utf8'), miniOpt));
                 break;
             // Ignore these file types
             case 'md':
                 break;
             // Copy all other file types
             default:
+            // fs.copyFileSync(`${devPath}${file}`, `${stgPath}${file}`);
         }
     }
+
+    const rmOpts = { recursive: true, force: true };
+    // Move files from stage to 'docs' folder
+    // fs.rmSync(`${dstPath}/${comp}`, rmOpts);
+    // fs.renameSync(`${stgPath}/${comp}`, `${dstPath}/${comp}`);
+    // // Tidy up
+    // fs.rmSync(stgPath, rmOpts);
 } catch (e) {
     if (e.cause && e.cause === 'custom') console.log(e.message);
     else console.log(e)
