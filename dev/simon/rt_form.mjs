ba31c16bff62d8b195faa2ec5490496cd1dccdcf
Sketchy <si@simonvickers.net>
2024-09-26 14:34:03 +0200

    convert modules to .mjs

---
+++ dev/simon/rt_form.mjs
@@ -0,0 +1,30 @@
+// ================================================================
+// Functions used used by <order-form> components
+
+//--- getStyle
+// Search if <style id="<component-name>"> has been defined in the upstream DOM (light or shadow).
+// If found then the node is cloned into the components shadow DOM to supercede the default styles.
+function getStyle(node) {
+    // Is <order-form> in the current Light DOM
+    const formNode = node.closest('order-form');
+
+    if (formNode !== null) {
+        // Terminate any iteration once the <order-form> node has been found in Light DOM
+        // Has a <style> element been defined in the datafile for this component
+        const styleNode = formNode.querySelector(`style#${node.nodeName.toLowerCase()}`);
+        // If found then recover style element else do nothing
+        if (styleNode) {
+            // Check if component has an existing style declaration
+            const existingStyleNode = node.shadowRoot.querySelector('style');
+            // Place 'external style' after existing style declaration
+            if (existingStyleNode) existingStyleNode.insertAdjacentElement('afterend', styleNode.cloneNode(true));
+            // ... Or place 'external style' before first element
+            else node.shadowRoot.childNodes[0].insertAdjacentElement('beforebegin', styleNode.cloneNode(true));
+        }
+    } else if (node.getRootNode() instanceof ShadowRoot) {
+        // If not found and root node is shadowRoot then continue search from shadowRoot's host element
+        return getStyle(node.getRootNode().host, name);
+    } else console.log('getStyle() - Something unexpected happened');
+}
+
+export { getStyle };
