9c0ea5163ab0e5d013f83a1900f263c96e33b942
Sketchy <si@simonvickers.net>
2024-10-15 15:43:29 +0200

    release latest changes

---
+++ docs/components/rt-orderform/index.js
@@ -0,0 +1,1 @@
+const moduleName=import.meta.url.split("/").slice(-2)[0],components=(console.time("loadModules for "+moduleName),["rt-menuitem","rt-itemdata","rt-lineitem","rt-pickup-locations","rt-form-field"]);Promise.all(components.map(o=>import(`../${o}/index.js`))).then(()=>{console.timeEnd("loadModules for "+moduleName),rtlib.loadComponent(import.meta.url)});
