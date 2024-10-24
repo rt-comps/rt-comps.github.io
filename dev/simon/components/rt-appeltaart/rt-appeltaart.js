811c267c9dc432720661b45ab0b32fb644fe4d67
Simon Vickers <simon.vickers@roads-technology.nl>
2024-10-24 11:34:57 +0200

    Apply new process to existing code

--- dev/simon/components/rt-appeltaart/rt-appeltaart.js
+++ dev/simon/components/rt-appeltaart/rt-appeltaart.js
@@ -1,12 +1,9 @@
 // ================================================================
-// === ati-form
+// === rt-appeltaart
 //
 // This is a container for an order form
-// It provides functions for loading the menu HTML and responds to a new order being placed
+// It provides code to load the menu HTML and respond to a new order being placed
 // 
-// Note: Template file exist, even though it is unnecessary, for rt.loadComponent() to function
-// Note 2: Form HTML must be loaded into shadowDOM for it to be rendered
-//
 const [compName, compPath] = rtlib.parseURL(import.meta.url);
 
 customElements.define(
@@ -23,56 +20,34 @@ customElements.define(
             this.#_sR = this.attachShadow({ mode: "open" });
             this.#_sR.append(this.$getTemplate())
 
-            // Get the menu file
-            this.getMenu();
-
+            //### Listeners
             // Catch form output event and display final order details
-            this.addEventListener('neworder', (e) => this.dispatchOrder(e));
+            this.addEventListener('neworder', (e) => this.#dispatchOrder(e));
+            // Wait for form to announce it has loaded
+            this.addEventListener('formready', (e) => this.#getMenu(e));
         }
-
-        //--- connectedCallback
-        //        connectedCallback() {
-        //        }
         //+++ End OF Lifecycle Events
 
-        //--- getMenu
-        // Retrieve the menu from file specified in the datafile attribute.
-        // If a relative path is provided then it should be relative to the <ati-form> element directory
-        getMenu() {
-            console.log(compPath);
-            console.log(compName);
-            // Check that the datafile attribute has been provided
+        //--- #getMenu
+        // Tell form to retrieve the specified menu data file
+        #getMenu(e) {
+            if (e) e.stopPropagation();
+            // Get the menu file
             if (this.hasAttribute('datafile') && this.getAttribute('datafile')) {
                 // Determing the path to the menu data file
-                const dataFile=this.getAttribute('datafile');
+                const dataFile = this.getAttribute('datafile');
                 const regex = /^http[s]?:\/\//;
-                const url = dataFile.match(regex)?`${dataFile}`:`${compPath}/${dataFile}`;
-                try {
-                    fetch(url)
-                        // Wait for the response
-                        .then((response) => {
-                            // Once response has been received, check for error
-                            if (!response.ok) throw `Failed to load ${url} with status ${response.status}`;
-                            return response.text()
-                        })
-                        //Wait for the text to be available
-                        .then((htmlText) => {
-                            // Create a fragment and then append to shadow DOM
-                            const frag = document.createRange().createContextualFragment(htmlText);
-                            this.#_sR.appendChild(frag);
-                        });
-                } catch (e) {
-                    console.warn(e);
-                }
+                const url = dataFile.match(regex) ? `${dataFile}` : `${compPath}/${dataFile}`;
+                this.#_sR.querySelector('rt-orderform').loadMenu(url);
             } else {
                 const frag = document.createRange().createContextualFragment('<h1 style="color: red;">datafile attribute not provided</h1>');
                 this.#_sR.appendChild(frag);
             }
         }
 
-        //--- dispatchOrder
+        //--- #dispatchOrder
         // Catch the neworder event from <rt-orderform> and process it as required
-        dispatchOrder(e) {
+        #dispatchOrder(e) {
             // Catch order and prevent further bubbling
             if (e) e.stopPropagation();
             // Process order
@@ -95,8 +70,8 @@ customElements.define(
                 fetch(handler, options)
                     .then((response) => { if (response.ok) return response.text(); })
                     .then((text) => {
-                        //document.querySelector('#Debug').innerHTML = text;
                         if (text.slice(-2) === 'OK' && text.slice(-3) !== 'NOK') {
+                            // Reset the order form if order sumbitted successfully
                             this.#_sR.querySelector('rt-orderform').accepted();
                         } else {
                             alert('Order submission failed');
