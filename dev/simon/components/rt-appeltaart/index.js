b5a7c2441a4704f09551f3dc79778b1d64189936
Simon Vickers <simon.vickers@roads-technology.nl>
2024-10-24 15:41:00 +0200

    Standardised code for index.js files

--- dev/simon/components/rt-appeltaart/index.js
+++ dev/simon/components/rt-appeltaart/index.js
@@ -1,40 +1,39 @@
 // ===== Import all required modules and components
 
-// --- loadGlobalMods()
+//--- loadGlobalMods()
 // Load base modules into global scope
-async function loadGlobalMods(basePath) {
+function loadGlobalMods(basePath) {
   // Define modules to load
-  // [
-  //    { label: <moduleName>, file: <moduleURL> },
-  //    ...
-  // ]
   const modules = [
     { label: 'rtlib', file: 'rt.mjs' },
     { label: 'rtBC', file: 'rt_baseclass.mjs' },
     { label: 'rtForm', file: 'rt_form.mjs' }
-  ]
-  // Simultaneously load modules into global scope, if not already present
-  return Promise.all(modules.map((module) => { if (typeof window[module.label] === 'undefined') import(`${basePath}/modules/${module.file}`).then((result) => window[module.label] = result) }))
+  ];
+  // Load any missing modules
+  return Promise.all(modules.map(async (module) => {
+    if (typeof window[module.label] === 'undefined') {
+      window[module.label] = await import(`${basePath}/modules/${module.file}`)
+        .catch(e => Promise.reject(`Failed to load '${module.file}' into '${module.label}'`));
+    } else return true;
+  }));
 }
 
-// --- Main
-// Store this file url as array
-const splitURL = import.meta.url.split('/');
-
-// Load any modules in to global scope, dependencies and then load component
-loadGlobalMods(splitURL.slice(0, -3).join('/')).then(() => {
-  const moduleName = splitURL.slice(-2)[0];
-  // Timer start (information only)
-  console.time(`loadModules for ${moduleName}`);
-  // List of dependancies
-  //  components will be loaded in parallel, so load order cannot be depended on.
-  const components = ['rt-orderform', 'rt-datepicker'];
-
-  // Load all required top-level components
-  Promise.all(components.map((componentName) => import(`../${componentName}/index.js`))).then(() => {
-    // Stop timer
-    console.timeEnd(`loadModules for ${moduleName}`);
-    // Load this component
-    rtlib.loadComponent(import.meta.url);
+//--- MAIN
+// Load any missing modules in to global scope, dependencies and then load component
+loadGlobalMods(import.meta.url.split('/').slice(0, -3).join('/'))
+  .then(() => {
+    // Timer start (information only)
+    const moduleName = import.meta.url.split('/').slice(-2)[0];
+    console.time(`loadModules for ${moduleName}`);
+    // List of dependancies
+    const components = ['rt-orderform', 'rt-datepicker'];
+    // Load all required top-level components
+    Promise.all(components.map((component) => import(`../${component}/index.js`)
+      .catch(e => console.error(`${e.message} ${moduleName.toUpperCase()} could not find ${component.toUpperCase()}`)))
+    ).then(() => {
+      // Stop timer
+      console.timeEnd(`loadModules for ${moduleName}`);
+      // Load this component
+      rtlib.loadComponent(import.meta.url);
+    });
   });
-})
