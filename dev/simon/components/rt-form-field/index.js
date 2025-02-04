f17914d998661f6df0c3a856707ad7e2c48d7070
Sketchy <si@simonvickers.net>
2025-02-04 16:33:52 +0100

    Cleanup index.js files

--- dev/simon/components/rt-form-field/index.js
+++ dev/simon/components/rt-form-field/index.js
@@ -1,17 +1,1 @@
-// ===== Import all required modules and components
-
-// ===== Import all required modules and components
-
-async function initialise(comp, options = {}) {
-    try {
-        // Load base module if not already loaded
-        if (typeof rtlib === 'undefined') window.rtlib = await import(`${comp.split('/').slice(0, -3).join('/')}/modules/rt.mjs`)
-        // Initialise component
-        rtlib.init(comp, options);
-    } catch (e) {
-        console.warn(e);
-    }
-}
-
-//--- MAIN
-initialise(import.meta.url);
+rtlib.init(import.meta.url);
--- dev/simon/components/rt-itemdata/index.js
+++ dev/simon/components/rt-itemdata/index.js
@@ -1,20 +1,6 @@
-// ===== Import all required modules and components
-
-async function initialise(comp, options = {}) {
-  try {
-    // Load base module if not already loaded
-    if (typeof rtlib === 'undefined') window.rtlib = await import(`${comp.split('/').slice(0, -3).join('/')}/modules/rt.mjs`)
-    // Initialise component
-    rtlib.init(comp, options);
-  } catch (e) {
-    console.warn(e);
-  }
-}
-
-//--- MAIN
 const options = {
   dependencies: [
     'rt-itemvariety'
   ]
 }
-initialise(import.meta.url, options);
+rtlib.init(import.meta.url, options);
