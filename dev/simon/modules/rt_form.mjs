4f68f331dfb8a83c1076b79931424a7318ab3b8a
Simon Vickers <simon.vickers@roads-technology.nl>
2025-03-18 13:16:07 +0100

    Improved getStyle() and added documentation

--- dev/simon/modules/rt_form.mjs
+++ dev/simon/modules/rt_form.mjs
@@ -1,42 +1,39 @@
 // ================================================================
 // Functions used used by <rt-orderform> components
 
+//--- findStyle
+// Search up the DOM (light or shadow) for <style id="<component-name>">.
+function findStyle(startNode, targetNodeName, startNodeName=startNode.nodeName) {
+    // See if targetNode is in the current LightDOM
+    const targetNode = startNode.closest(targetNodeName);
+
+    // If found then return style node (null if style node doesn't exist)
+    if (targetNode !== null) return targetNode.querySelector(`form-config style#${startNodeName.toLowerCase()}`)
+    // If not found BUT root node is ShadowRoot then recurse out of ShadowDOM
+    else if (startNode !== targetNode && startNode.getRootNode() instanceof ShadowRoot) return findStyle(startNode.getRootNode().host, targetNodeName, startNodeName)
+    // If not found AND root node is not ShadowRoot then terminate as this means that the tergetNode was not found on this DOM branch!
+    else return null;
+}
+
 //--- getStyle
-// Search if <style id="<component-name>"> has been defined in the upstream DOM (light or shadow).
-// If found then the node is cloned into the components shadow DOM to supercede the default styles.
-function getStyle(node) {
-    // Set '???' to component node name of interest to deug errors
-    const debug = node.nodeName.toUpperCase() === '???' ? true : false;
-    // Is <rt-orderform> in the current Light DOM
-    const formNode = node.closest('rt-orderform');
-    if (debug) {
-        console.log(formNode);
-        console.log(node.getRootNode());
-        console.log(node.getRootNode().host);
-        setTimeout(()=> {
-            console.log('After Timeout');
-            console.log(node.getRootNode());
-            const formNode2 = node.closest('rt-orderform');
-            console.log(node.getRootNode().host)
-        }, 1000)
+// If <style id="<component-name>"> has been defined in the upstream DOM (light or shadow) then
+// Cloned the node into the components shadow DOM after the default <style> node to supercede it.
+function getStyle(node, topNode = 'rt-orderform') {
+    // Set '???' to component node name of interest to debug errors
+    //const debug = node.nodeName.toUpperCase() === 'RT-LINEITEM' ? true : false;
+    //if (debug) console.log(something);
+    // Look for 'topNode' in the current DOM
+    const styleNode = findStyle(node, topNode);
+
+    if (styleNode !== null) {
+        console.log(`Found style node for ${node.nodeName}`);
+        // Check if component has an existing style declaration
+        const existingStyleNode = node.shadowRoot.querySelector('style');
+        // Place 'external style' after existing style declaration
+        if (existingStyleNode) existingStyleNode.insertAdjacentElement('afterend', styleNode.cloneNode(true));
+        // ... Or place 'external style' before first element
+        else node.shadowRoot.childNodes[0].insertAdjacentElement('beforebegin', styleNode.cloneNode(true));
     }
-    if (formNode !== null) {
-        // Terminate any iteration once the <rt-orderform> node has been found in Light DOM
-        // Has a <style> element been defined in the datafile for this component
-        const styleNode = formNode.querySelector(`style#${node.nodeName.toLowerCase()}`);
-        // If found then recover style element else do nothing
-        if (styleNode) {
-            // Check if component has an existing style declaration
-            const existingStyleNode = node.shadowRoot.querySelector('style');
-            // Place 'external style' after existing style declaration
-            if (existingStyleNode) existingStyleNode.insertAdjacentElement('afterend', styleNode.cloneNode(true));
-            // ... Or place 'external style' before first element
-            else node.shadowRoot.childNodes[0].insertAdjacentElement('beforebegin', styleNode.cloneNode(true));
-        }
-    } else if (node.getRootNode() instanceof ShadowRoot) {
-        // If not found and root node is shadowRoot then continue search from shadowRoot's host element
-        return getStyle(node.getRootNode().host);
-    } else console.log(`getStyle() - Something unexpected happened for ${node.nodeName.toUpperCase()}`);
 }
 
 export { getStyle };
