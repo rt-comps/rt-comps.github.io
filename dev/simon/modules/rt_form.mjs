b5b183125ab88bbc6b9a9099abec9dbcfc64ea2b
Sketchy <si@simonvickers.net>
2025-03-18 16:33:07 +0100

    testing for edge cases and tidy up

--- dev/simon/modules/rt_form.mjs
+++ dev/simon/modules/rt_form.mjs
@@ -4,20 +4,25 @@
 //--- findStyle
 // Search up the DOM (light or shadow) for <style id="<component-name>">.
 function findStyle(startNode, targetNodeName, startNodeName=startNode.nodeName) {
-    // See if targetNode is in the current LightDOM
+    // See if targetNode is in this component's 'LightDOM'
     const targetNode = startNode.closest(targetNodeName);
-
-    // If found then return style node (null if style node doesn't exist)
-    if (targetNode !== null) return targetNode.querySelector(`form-config style#${startNodeName.toLowerCase()}`)
+    // No need to do anything in this case as any matching STYLE will already have been applied
+    if (targetNode === startNode) return null
+    
+    // If found then return value of query for style node (null if style node not present)
+    if (targetNode !== null)
+        return targetNode.querySelector(`form-config style#${startNodeName.toLowerCase()}`)
     // If not found BUT root node is ShadowRoot then recurse out of ShadowDOM
-    else if (startNode !== targetNode && startNode.getRootNode() instanceof ShadowRoot) return findStyle(startNode.getRootNode().host, targetNodeName, startNodeName)
+    else if (startNode.getRootNode() instanceof ShadowRoot) 
+        return findStyle(startNode.getRootNode().host, targetNodeName, startNodeName)
     // If not found AND root node is not ShadowRoot then terminate as this means that the tergetNode was not found on this DOM branch!
     else return null;
+    
 }
 
 //--- getStyle
 // If <style id="<component-name>"> has been defined in the upstream DOM (light or shadow) then
-// Cloned the node into the components shadow DOM after the default <style> node to supercede it.
+// Cloned the node into the components shadow DOM after the default <style> node to override entries in it.
 function getStyle(node, topNode = 'rt-orderform') {
     // Set '???' to component node name of interest to debug errors
     //const debug = node.nodeName.toUpperCase() === 'RT-LINEITEM' ? true : false;
@@ -25,8 +30,8 @@ function getStyle(node, topNode = 'rt-orderform') {
     // Look for 'topNode' in the current DOM
     const styleNode = findStyle(node, topNode);
 
+    // Apply found style to ShadowDOM
     if (styleNode !== null) {
-        console.log(`Found style node for ${node.nodeName}`);
         // Check if component has an existing style declaration
         const existingStyleNode = node.shadowRoot.querySelector('style');
         // Place 'external style' after existing style declaration
