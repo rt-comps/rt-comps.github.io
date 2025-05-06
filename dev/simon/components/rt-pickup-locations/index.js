083fe8fccb171273f47c8cef1067545007fe6e5b
Sketchy <si@simonvickers.net>
2025-05-06 15:37:51 +0200

    Fixed import string

--- dev/simon/components/rt-pickup-locations/index.js
+++ dev/simon/components/rt-pickup-locations/index.js
@@ -2,7 +2,7 @@
 
 try {
   // Load base module if not already loaded
-  if (typeof rtlib === 'undefined') window.rtlib = await import(`${comp.split('/').slice(0, -3).join('/')}/modules/rt.mjs`)
+  if (typeof rtlib === 'undefined') window.rtlib = await import(`${import.meta.url.split('/').slice(0, -3).join('/')}/modules/rt.mjs`)
   // Initialise component
   rtlib.init(import.meta.url);
 } catch (e) {
